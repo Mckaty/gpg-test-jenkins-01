12992d480a0e00a80a2599fdaf24f2436e9914a9
Mckaty <mckaty.gm@gmail.com>
2023-07-03 14:42:42 +0700

    Add files via upload
    new script pgp

---
+++ gpg-decryption-s3-uat.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Set your AWS access key ID and secret access key
+#export AWS_ACCESS_KEY_ID="your-access-key-id"
+#export AWS_SECRET_ACCESS_KEY="your-secret-access-key"
+
+# Set the S3 bucket name and prefix for the objects
+S3_BUCKET="poc-gpg-bucket-uat-142603072023"
+#OBJECT_PREFIX="in/"
+#OBJECT_PREFIX_AFTER="out/"
+S3_INPUT_FOLDER="in/"
+S3_OUTPUT_FOLDER="out/"
+
+
+# Set the local directory where decrypted files will be saved
+LOCAL_DIR="/tmp/decrypted-files-uat"
+
+# Download the encrypted files from S3
+aws s3 cp "s3://${S3_BUCKET}/${S3_INPUT_FOLDER}" /tmp --recursive --exclude "*" --include "*.pgp"
+
+# Decrypt each file using gpg
+for file in /tmp/*.pgp; do
+  filename=$(basename "$file")
+  output_file="${LOCAL_DIR}${filename%.pgp}"
+  gpg --decrypt --output "$output_file" "$file"
+
+# Upload the decrypted file to S3
+  aws s3 cp "$output_file" "s3://${S3_BUCKET}/${S3_OUTPUT_FOLDER}${filename%.pgp}"
+done
+
+# Remove the temporary encrypted files
+#rm /tmp/decrypted-files-uat/*.pgp
+
+# Display a success message
+echo "Decryption complete. Files saved in ${LOCAL_DIR}"
